ad0e1e3d99aea7addb8e9ca8ae200922f3ca86a7
Julien BERTRAND <j.bertrand.sin@gmail.com>
2024-07-08 16:34:25 +0200

    fix: fix

--- VR-Isitech/Assets/Scenes/Scène D/tirCanon1.cs
+++ VR-Isitech/Assets/Scenes/Scène D/tirCanon1.cs
@@ -4,27 +4,40 @@ using UnityEngine;
 
 public class tirCanon1 : MonoBehaviour
 {
+    public Animator currentAnimator;  // Référence à l'Animator
+    public GameObject correctObject;  // L'objet qui déclenchera l'animation
 
-    public Animator currentAnimator;
-    public GameObject correctObject;
     // Start is called before the first frame update
     void Start()
     {
-        currentAnimator = gameObject.GetComponent<Animator>();
+        // Vérifie si l'Animator est déjà assigné, sinon essaie de le récupérer
+        if (currentAnimator == null)
+        {
+            currentAnimator = gameObject.GetComponent<Animator>();
+            if (currentAnimator == null)
+            {
+                Debug.LogError("Animator not found on " + gameObject.name);
+            }
+        }
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Pas de code nécessaire ici pour le moment
     }
+
+    // Méthode appelée lorsque le collider entre en collision avec un autre collider
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("out of if but trigger");
+        Debug.Log("Collider entered trigger");
+
+        // Vérifie si l'objet entrant est celui attendu
         if (other.gameObject == correctObject)
         {
-            Debug.Log("is trigger");
-            currentAnimator.SetBool("trigger", true);
+            Debug.Log("Correct object triggered animation");
+            currentAnimator.SetBool("trigger", true);  // Déclenche l'animation
         }
     }
 }
