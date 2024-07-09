a5ccd331a25eb20bf95d2d61f7658db8bfbf1337
JulienCarrillo <neyrishdev@gmail.com>
2024-07-09 15:28:32 +0200

    feat: puzzle

--- VR-Isitech/Assets/VRTemplateAssets/Scripts/DestroyObject.cs
+++ VR-Isitech/Assets/VRTemplateAssets/Scripts/DestroyObject.cs
@@ -10,6 +10,7 @@ public class DestroyOnContact : MonoBehaviour
     // Méthode appelée lorsqu'un autre collider entre en contact avec celui de cet objet
     private void OnCollisionEnter(Collision collision)
     {
+        Debug.Log("an collison was make ");
         // Vérifie si l'objet en contact a le tag de l'arme
         if (collision.gameObject.CompareTag(weaponTag))
         {
