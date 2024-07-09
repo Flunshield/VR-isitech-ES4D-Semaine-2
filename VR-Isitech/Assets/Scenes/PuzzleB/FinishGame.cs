4406f680b5b56e6ec670faaf1ed998df3e6b14bb
Ilhan <ilhan.koprulu1@gmail.com>
2024-07-09 15:27:08 +0200

    Ajout particule

--- VR-Isitech/Assets/Scenes/PuzzleB/FinishGame.cs
+++ VR-Isitech/Assets/Scenes/PuzzleB/FinishGame.cs
@@ -5,6 +5,7 @@ public class FinishGame : MonoBehaviour
 
     public GameObject Sphere;
     public GameObject Portail;
+    public GameObject particule;
     void OnTriggerEnter(Collider other)
     {
        Debug.Log("there it is ");
@@ -13,8 +14,16 @@ public class FinishGame : MonoBehaviour
             if (Portail != null)
             {
                 Portail.SetActive(true);
+                Sphere.SetActive(false);
                 Debug.Log("Portal object activated.");
+                particule.SetActive(true);
+                StartCoroutine(DeactivateAfterDelay(1f));
             }
         }
+    }
+    System.Collections.IEnumerator DeactivateAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        particule.SetActive(false);
     }
 }
