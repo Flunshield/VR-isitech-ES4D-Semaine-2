f27351b69f51ec974b28b589a53c18c6242e5d99
Kévin Begot <begot.kevin@gmail.com>
2024-07-09 12:12:08 +0200

    [ADD] dynamic portal

---
+++ VR-Isitech/Assets/Scenes/puzzleA/receptacleB.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class ReceptacleB : MonoBehaviour
+{
+    public GameObject correctObject;
+    public Material triggeredMaterial;
+    public GameObject portal;
+    private Material originalMaterial;
+    private bool objectPlaced = false;
+
+    void Start()
+    {
+        originalMaterial = GetComponent<Renderer>().material;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!objectPlaced && other.gameObject == correctObject)
+        {
+            objectPlaced = true;
+            Rigidbody rb = other.gameObject.GetComponent<Rigidbody>();
+            if (rb != null)
+            {
+                rb.isKinematic = true; // Set isKinematic to true
+            }
+
+            other.gameObject.GetComponent<XRGrabInteractable>().enabled = false;
+            other.gameObject.transform.position = transform.position;
+            other.gameObject.transform.rotation = transform.rotation;
+            GetComponent<Renderer>().material = triggeredMaterial;
+        }
+        if (portal != null)
+        {
+            portal.SetActive(true);
+        }
+    }
+}
