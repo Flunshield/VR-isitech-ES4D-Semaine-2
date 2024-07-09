62e4f4dec86242c669a1fde49c922fc31e1df618
Ilhan <ilhan.koprulu1@gmail.com>
2024-07-09 14:45:58 +0200

    Finalisation

--- VR-Isitech/Assets/Scenes/PuzzleB/FinishGame.cs
+++ VR-Isitech/Assets/Scenes/PuzzleB/FinishGame.cs
@@ -4,12 +4,15 @@ public class FinishGame : MonoBehaviour
 {
     void OnCollisionEnter(Collision collision)
     {
-        // Vérifiez si l'objet avec lequel nous sommes entrés en collision a le tag "Collectible"
         if (collision.gameObject.CompareTag("Finish"))
         {
             Debug.Log("Collision with Finish detected!");
-            // Ajoutez votre logique ici, par exemple détruire l'objet collecté
-            Destroy(collision.gameObject);
+            GameObject respawnObject = GameObject.FindGameObjectWithTag("Respawn");
+            if (respawnObject != null)
+            {
+                respawnObject.SetActive(true);
+                Debug.Log("Portal object activated.");
+            }
         }
     }
 }
