d66bd81655c16961685bbe2900bc6e20691aec4e
JulienCarrillo <neyrishdev@gmail.com>
2024-07-10 13:16:42 +0200

    feat: receptacle available

--- VR-Isitech/Assets/Scenes/Scène B/Receptacle.cs
+++ VR-Isitech/Assets/Scenes/Scène B/Receptacle.cs
@@ -4,13 +4,21 @@ using UnityEngine.XR.Interaction.Toolkit;
 public class Receptacle : MonoBehaviour
 {
     public GameObject correctObject;
+    public GameObject displayObject;
     public Material triggeredMaterial;
     private Material originalMaterial;
     private bool objectPlaced = false;
 
+    private GlobalReceptacle globalReceptacle; // Référence au script GlobalReceptacle
+
     void Start()
     {
         originalMaterial = GetComponent<Renderer>().material;
+    }
+
+    public void SetGlobalReceptacle(GlobalReceptacle receptacle)
+    {
+        globalReceptacle = receptacle;
     }
 
     private void OnTriggerEnter(Collider other)
@@ -22,12 +30,23 @@ public class Receptacle : MonoBehaviour
             if (rb != null)
             {
                 rb.isKinematic = true; // Set isKinematic to true
+                displayObject.SetActive(true);
             }
 
             other.gameObject.GetComponent<XRGrabInteractable>().enabled = false;
             other.gameObject.transform.position = transform.position;
             other.gameObject.transform.rotation = transform.rotation;
             GetComponent<Renderer>().material = triggeredMaterial;
+
+            if (globalReceptacle != null)
+            {
+                globalReceptacle.CheckReceptacles();
+            }
         }
+    }
+
+    public bool IsObjectPlaced()
+    {
+        return objectPlaced;
     }
 }
---
+++ VR-Isitech/Assets/globalReceptacle.cs
@@ -0,0 +1,47 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalReceptacle : MonoBehaviour
+{
+    public List<Receptacle> receptacles;
+    public LeftDoorController leftFirstDoorController;  
+    public RightDoorController rightFirstDoorController;  
+    public LeftDoorController leftSecondDoorController;  
+    public RightDoorController rightSecondDoorController;
+
+    void Start()
+    {
+        foreach (var receptacle in receptacles)
+        {
+            receptacle.SetGlobalReceptacle(this);
+        }
+    }
+
+    // Méthode pour vérifier l'état des réceptacles
+    public void CheckReceptacles()
+    {
+        int placedCount = 0;
+        foreach (var receptacle in receptacles)
+        {
+            if (receptacle.IsObjectPlaced())
+            {
+                placedCount++;
+            }
+        }
+
+        if (placedCount >= 3)
+        {
+            leftFirstDoorController.OpenDoors();
+            rightFirstDoorController.OpenDoors();
+            leftSecondDoorController.OpenDoors();
+            rightSecondDoorController.OpenDoors();
+        }
+        else
+        {
+            leftFirstDoorController.CloseDoors();
+            rightFirstDoorController.CloseDoors();
+            leftSecondDoorController.CloseDoors();
+            rightSecondDoorController.CloseDoors();
+        }
+    }
+}
