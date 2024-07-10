fd99081e7619a86a411321bffe62e797ea97e32e
JulienCarrillo <neyrishdev@gmail.com>
2024-07-10 12:05:51 +0200

    feat: logique et decors

--- VR-Isitech/Assets/LeftDoorController.cs
+++ VR-Isitech/Assets/LeftDoorController.cs
@@ -7,7 +7,9 @@ public class LeftDoorController : MonoBehaviour
     public Transform leftDoorTransform;   // Référence à la transform de la porte gauche
     public float openAngle = 90f;         // Angle d'ouverture de chaque porte
     public float openSpeed = 2f;          // Vitesse d'ouverture des portes
-    public MeshCollider doorCollider;     // Référence au MeshCollider de la porte
+    public MeshCollider doorCollider; 
+
+    public MeshCollider doorFrames;
 
     private bool isOpen = false;          // État actuel des portes (ouvertes/fermées)
     private Quaternion leftClosedRotation;  // Rotation initiale de la porte gauche
@@ -25,11 +27,13 @@ public class LeftDoorController : MonoBehaviour
         {
             leftDoorTransform.rotation = Quaternion.Slerp(leftDoorTransform.rotation, leftOpenRotation, Time.deltaTime * openSpeed);
             doorCollider.enabled = false; // Désactiver le collider lorsque la porte est ouverte
+            doorFrames.enabled = false;
         }
         else
         {
             leftDoorTransform.rotation = Quaternion.Slerp(leftDoorTransform.rotation, leftClosedRotation, Time.deltaTime * openSpeed);
-            doorCollider.enabled = true; // Activer le collider lorsque la porte est fermée
+            doorCollider.enabled = true;
+            doorFrames.enabled = false; // Activer le collider lorsque la porte est fermée
         }
     }
 
--- VR-Isitech/Assets/RightDoorController.cs
+++ VR-Isitech/Assets/RightDoorController.cs
@@ -8,7 +8,7 @@ public class RightDoorController : MonoBehaviour
     public float openAngle = 90f;         // Angle d'ouverture de chaque porte
     public float openSpeed = 2f;          // Vitesse d'ouverture des portes
     public MeshCollider doorCollider;     // Référence au MeshCollider de la porte
-
+    public MeshCollider doorFrames;
     private bool isOpen = false;          // État actuel des portes (ouvertes/fermées)
     private Quaternion rightClosedRotation;  // Rotation initiale de la porte droite
     private Quaternion rightOpenRotation;    // Rotation finale de la porte droite
@@ -25,11 +25,13 @@ public class RightDoorController : MonoBehaviour
         {
             rightDoorTransform.rotation = Quaternion.Slerp(rightDoorTransform.rotation, rightOpenRotation, Time.deltaTime * openSpeed);
             doorCollider.enabled = false; // Désactiver le collider lorsque la porte est ouverte
+            doorFrames.enabled = false;
         }
         else
         {
             rightDoorTransform.rotation = Quaternion.Slerp(rightDoorTransform.rotation, rightClosedRotation, Time.deltaTime * openSpeed);
             doorCollider.enabled = true; // Activer le collider lorsque la porte est fermée
+            doorFrames.enabled = true;
         }
     }
 
